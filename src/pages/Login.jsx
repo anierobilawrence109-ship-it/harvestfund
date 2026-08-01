322df4084b5e2beb469bb19fc95a33696335ef36
Lawrence Anierobi <anierobilawrence109@gmail.com>
2026-08-02 00:22:13 +0100

    Fixed login API URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,8 +13,8 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch(
-        "http://localhost:5000/api/auth/login",
+     const response = await fetch(
+  "https://harvestfund.onrender.com/api/auth/login",
         {
           method: "POST",
           headers: {
